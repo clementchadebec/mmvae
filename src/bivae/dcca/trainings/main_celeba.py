added the linear cca to the dcca encoding to fix the scaling issue

src/bivae/dcca/trainings/main_celeba.py
@@ -191,7 +191,7 @@ if __name__ == '__main__':
 
     # if a linear CCA should get applied on the learned features extracted from the networks
     # it does not affect the performance on noisy MNIST significantly
-    apply_linear_cca = False
+    apply_linear_cca = True
     # end of parameters section
     ############
 
@@ -205,7 +205,11 @@ if __name__ == '__main__':
                     learning_rate, reg_par, device=device)
 
     solver.fit(train_loader, checkpoint=save_to)
-    # TODO: Save l_cca model if needed
+
+    # Save parameters for the lcca
+    print(l_cca.w[0].shape)
+    np.save(str(save_to) + '/l_cca_w.npy', l_cca.w)
+    np.save(str(save_to) + '/l_cca_m.npy', l_cca.m)
 
 
     # Training and testing of SVM with linear kernel on the view 1 with new features

src/bivae/models/nn/encoders.py
@@ -175,7 +175,7 @@ class TwoStepsDecoder(BaseDecoder):
 
 class TwoStepsEncoder(BaseEncoder):
     """Defines a two-step encoder, with the first step being pretrained and requires no grad"""
-    def __init__(self, pretrained_encoder,args):
+    def __init__(self, pretrained_encoder,args, hidden_dim=512, num_hidden=3):
 
         BaseEncoder.__init__(self)
         self.first_encoder = pretrained_encoder
@@ -200,6 +200,7 @@ class TwoStepsEncoder(BaseEncoder):
 
         with torch.no_grad():
             h = self.first_encoder(x).embedding
+
         h1 = self.sec_encoder(h)
         out = ModelOutput(
             embedding = self.embedding(h1),