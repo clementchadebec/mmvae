0166a911f06e451189e0a8bb58ed223b2574def7
AgatheSenellart <agathe.senellart@polytechnique.edu>
2022-12-01 09:53:16 +0100

    small change

--- src/bivae/validate.py
+++ src/bivae/validate.py
@@ -40,7 +40,7 @@ random.seed(args.seed)
 # Log parameters of the experiments
 experiment_name = args.wandb_experiment if hasattr(args,'wandb_experiment') else args.model
 
-wandb.init(project = experiment_name , entity="multimodal_vaes") 
+wandb.init(project = experiment_name , entity="asenellart") 
 wandb.config.update(args)
 wandb.define_metric('epoch')
 wandb.define_metric('*', step_metric='epoch')
