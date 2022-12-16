a31df006e987f64eaa44b28fc4e9792b6cc81230
AgatheSenellart <agathe.senellart@polytechnique.edu>
2022-12-16 17:49:54 +0100

    reduced dcca medmnist

--- src/bivae/dcca/models/medmnist.py
+++ src/bivae/dcca/models/medmnist.py
@@ -1,10 +1,12 @@
+import numpy as np
 import torch
 import torch.nn as nn
-from ..objectives import cca_loss
-from pythae.models import VAE_LinNF_Config, VAE_IAF_Config, VAEConfig
-from bivae.my_pythae.models import my_VAE, my_VAE_LinNF, my_VAE_IAF
-from bivae.models.nn.medmnist import Encoder_ResNet_AE_medmnist, Encoder_Resnet18_AE_medmnist
+from pythae.models import VAEConfig
+from pythae.models.base.base_utils import ModelOutput
 
+from bivae.models.nn.medmnist import (Encoder_ResNet_AE_medmnist)
+
+from ..objectives import cca_loss
 
 encoder = Encoder_ResNet_AE_medmnist
 
@@ -35,15 +37,62 @@ class DeepCCA_MedMNIST(nn.Module):
         output2 = self.model2(x2).embedding
 
         return output1, output2
+    
+    
+
+class wrapper_encoder_lcca_model1(nn.Module):
+
+    def __init__(self, dim):
+        super(wrapper_encoder_lcca_model1, self).__init__()
+        # get the outdim size of the encoders from the json file
+
+        model1 = encoder(VAEConfig((1,28,28), 16))
+        model1.load_state_dict(torch.load('../experiments/dcca/mnist_svhn/model1.pt'))
+        self.latent_dim = dim
+
+        self.encoder = model1
+        self.m = np.load('../experiments/dcca/medmnist/l_cca_m.npy')[0]
+        self.m = torch.tensor(self.m).cuda().float()
+        self.w = np.load('../experiments/dcca/medmnist/l_cca_w.npy')[0]
+        self.w = torch.tensor(self.w).cuda().float()
+        
+    def forward(self, x):
+        h = self.encoder(x)['embedding']
+        result = h - self.m.reshape([1, -1]).repeat(len(h), 1)
+        result = torch.mm(result, self.w)
+        o = ModelOutput(embedding = result.float()[:,:self.latent_dim])
+        return o
+
+class wrapper_encoder_lcca_model2(nn.Module):
+
+    def __init__(self, dim):
+        super(wrapper_encoder_lcca_model2, self).__init__()
+
+        model2 = encoder(VAEConfig((3,28,28), 16))
+        model2.load_state_dict(torch.load('../experiments/dcca/mnist_svhn/model2.pt'))
+        self.latent_dim = dim
+
+        self.encoder = model2
+        self.m = np.load('../experiments/dcca/medmnist/l_cca_m.npy')[1]
+        self.m = torch.tensor(self.m).cuda().float()
+        self.w = np.load('../experiments/dcca/medmnist/l_cca_w.npy')[1]
+        self.w = torch.tensor(self.w).cuda().float()
+
+    def forward(self, x):
+        self.encoder.eval()
+        h = self.encoder(x)['embedding']
+        result = h - self.m.reshape([1, -1]).repeat(len(h), 1)
+        result = torch.mm(result, self.w)
+
+        o = ModelOutput(embedding = result.float()[:,:self.latent_dim])
+        return o
+
 
 
 def load_dcca_medmnist(outdim_size):
 
-    model1 = encoder(VAEConfig((1,28,28), outdim_size))
-    model2 = encoder(VAEConfig((3,28,28), outdim_size))
-
-    model1.load_state_dict(torch.load('../experiments/dcca/medmnist/model1.pt'))
-    model2.load_state_dict(torch.load('../experiments/dcca/medmnist/model2.pt'))
+    model1 = wrapper_encoder_lcca_model1(outdim_size)
+    model2 = wrapper_encoder_lcca_model2(outdim_size)
 
     return [model1, model2]
 
